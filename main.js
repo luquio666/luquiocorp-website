aa7a9ad06d2bd478d22a079b68784d77ef670dc9
Lucio Misael Aquino <luq.notperfect@gmail.com>
2022-09-21 01:49:03 -0300

    Added terminal view and main functionality. Added topbar with basic functionality

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function onDivClick(id) {
         
         const hasClass = document.getElementById(id).classList.contains('show')
 
+        document.getElementById("topBar-left").innerText = id.toUpperCase();
+
         // Agregamos el zindex en base al segundo actual asi la
         // ultima ventana se pone arriba que la anterior
         // hay que usar substring porque zindex no acepta numeros tan grandes.
@@ -13,4 +15,16 @@ function onDivClick(id) {
             ? document.getElementById(id).classList.remove('show')
             : document.getElementById(id).classList.add('show')
     }
+}
+
+function init()
+{
+    showRealTime();
+}
+
+function showRealTime() {
+    var date = new Date();
+    var dateFormated = date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+    document.getElementById("topBar-right").innerText = dateFormated;
+    var reset = setTimeout(showRealTime, 500);
 }
